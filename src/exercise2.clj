321cb1dd493f0d5e26906e0aaaa2139d9dcfe1ef
ordonezf <ordonez.f93@gmail.com>
2017-08-18 21:22:50 -0300

    style fixes

--- src/exercise2.clj
+++ src/exercise2.clj
@@ -1,7 +1,6 @@
 (ns exercise2)
 
 (defn only-greater-than-five
-    "filter numbers lower than 5"
-    [coll]
-    (filter (fn [x] (< 5 x)) coll)
-)
+  "filter numbers lower than 5"
+  [coll]
+  (filter (fn [x] (< 5 x)) coll))
